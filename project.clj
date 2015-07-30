66bc983a0a16123fa1ceaeb6398f20dea4700985
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-30 21:43:17 +0800

    project file

---
+++ project.clj
@@ -0,0 +1,24 @@
+(defproject infinitelives.utils "0.1.0-SNAPSHOT"
+  :description "Support utilities for cljs games"
+  :url "https://github.com/infinitelives"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "0.0-3308"]]
+
+  :plugins [[lein-cljsbuild "1.0.6"]]
+
+  :source-paths ["src/cljs"]
+  :test-paths ["test"]
+
+  :cljsbuild
+  {
+   :builds {:test
+            {:source-paths ["src" "test"]
+             :notify-command []
+             :compiler {:output-to "resources/test/compiled.js"
+                        :optimizations :whitespace
+                        :pretty-print true}}}
+   :test-commands {"test" ["phantomjs"
+                           "resources/test/test.js"
+                           "resources/test/test.html"]}})
