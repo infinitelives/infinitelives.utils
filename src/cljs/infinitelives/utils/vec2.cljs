ff153c52c259fff404437a865515dbe15042e01c
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-30 22:21:34 +0800

    magnitude and distance vec2 functions

--- src/cljs/infinitelives/utils/vec2.cljs
+++ src/cljs/infinitelives/utils/vec2.cljs
@@ -18,3 +18,30 @@
   ^{:arglist '([])
    :doc "Return the zero two dimensional vector #<0,0>"}
   zero Vec2/createFloat64)
+
+(def
+  ^{:arglist '([v])
+    :doc "Return the length of vector v"}
+  magnitude Vec2/magnitude)
+
+(def
+  ^{:arglist '([v])
+    :doc "Return the length squared of vector v"}
+  magnitude-squared Vec2/magnitudeSquared)
+
+(def
+  ^{:arglist '([v0 v1])
+    :doc "Return the distance between the tip of v0 and the tip of v1"}
+  distance Vec2/distance)
+
+(def
+  ^{:arglist '([v0 v1])
+    :doc "Return the distance squared between the tip of v0 and the tip of v1"
+    }
+  distance-squared Vec2/distanceSquared)
+
+(def
+  ^{:arglist '([v0 v1])
+    :doc "Returns true is both v0 and v1 point in the same direction
+     and are of the same length"}
+  equals Vec2/equals)
