f83597da529961ecd0364da52dbc3b4bc28e4513
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-31 18:22:07 +0800

    test suite for new vec2 functions

--- test/infinitelives/utils/vec2_test.cljs
+++ test/infinitelives/utils/vec2_test.cljs
@@ -65,3 +65,95 @@
   (is (vec2/equals
        (vec2/vec2 1 1)
        (vec2/sub (vec2/vec2 20 -10) (vec2/vec2 19 -11)))))
+
+(deftest dot
+  (is (= (vec2/dot
+          (vec2/vec2 10 20)
+          (vec2/vec2 5 2))
+         90)))
+
+(deftest scale
+  (is (vec2/equals (vec2/scale (vec2/vec2 1 -1) 5)
+                  (vec2/vec2 5 -5))))
+
+(deftest unit
+  (is (vec2/equals
+       (vec2/unit (vec2/vec2 10 -10))
+       (vec2/vec2 on-root-2 (- on-root-2)))))
+
+(deftest abs
+  (is (vec2/equals
+       (vec2/abs (vec2/vec2 -2 -5))
+       (vec2/vec2 2 5))))
+
+(deftest direction
+  (is (vec2/equals
+       (vec2/direction (vec2/vec2 10 5) (vec2/vec2 10 -5))
+       (vec2/vec2 0 -1))))
+
+(deftest rotate
+  (is (vec2/almost
+       (vec2/rotate (vec2/vec2 1 0) pi-on-2)
+       (vec2/vec2 0 1)))
+  (is (vec2/almost
+       (vec2/rotate (vec2/vec2 20 20) Math/PI)
+       (vec2/vec2 -20 -20)))
+  (is (vec2/almost
+       (vec2/rotate (vec2/vec2 10 10) (- pi-on-2))
+       (vec2/rotate (vec2/vec2 10 10) (* 3 pi-on-2))
+       (vec2/vec2 10 -10))))
+
+(deftest random-unit
+  (loop [tests 20]
+    (let [v1 (vec2/random-unit)
+          v2 (vec2/random-unit)]
+      (is (<= 0.9999999 (vec2/magnitude v1) 1))
+      (is (<= 0.9999999 (vec2/magnitude v2) 1))
+      (is (not (vec2/almost v1 v2)))
+      (when (pos? tests)
+        (recur (dec tests))))))
+
+(deftest random
+  (loop [tests 20]
+    (let [v1 (vec2/random)
+          v2 (vec2/random)]
+      (is (<= (vec2/magnitude v1) 1))
+      (is (<= (vec2/magnitude v2) 1))
+      (is (not (vec2/almost v1 v2)))
+      (when (pos? tests)
+        (recur (dec tests))))))
+
+(deftest lerp
+  (let [v0 (vec2/vec2 -10 -10)
+        v1 (vec2/vec2 10 10)]
+    (is (vec2/equals
+         (vec2/lerp v0 v1 0)
+         v0))
+    (is (vec2/equals
+         (vec2/lerp v0 v1 1)
+         v1))
+    (is (vec2/equals
+         (vec2/lerp v0 v1 0.5)
+         (vec2/zero)))
+    (is (vec2/equals
+         (vec2/lerp v0 v1 0.25)
+         (vec2/vec2 -5 -5)))
+    (is (vec2/equals
+         (vec2/lerp v0 v1 0.75)
+         (vec2/vec2 5 5)))
+    (is (vec2/equals
+         (vec2/lerp v0 v1 2)
+         (vec2/vec2 30 30)))
+    (is (vec2/equals
+         (vec2/lerp v0 v1 -1)
+         (vec2/vec2 -30 -30)))))
+
+(deftest heading
+  (is (= 0 (vec2/heading (vec2/vec2 1 0))))
+  (is (= pi-on-2 (vec2/heading (vec2/vec2 0 1))))
+  (is (= Math/PI (vec2/heading (vec2/vec2 -1 0))))
+  (is (= (* 3 pi-on-2) (vec2/heading (vec2/vec2 0 -1))))
+  (is (= (/ Math/PI 4) (vec2/heading (vec2/vec2 1 1))))
+  (is (= (* 3 (/ Math/PI 4)) (vec2/heading (vec2/vec2 -1 1))))
+  (is (= (* 5 (/ Math/PI 4)) (vec2/heading (vec2/vec2 -1 -1))))
+  (is (= (* 7 (/ Math/PI 4)) (vec2/heading (vec2/vec2 1 -1)))))
