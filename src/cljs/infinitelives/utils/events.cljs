4e3817b0072c6470fe9fb86dba7881eee04a1398
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-16 12:10:05 +0800

    and frame-channel

--- src/cljs/infinitelives/utils/events.cljs
+++ src/cljs/infinitelives/utils/events.cljs
@@ -254,3 +254,30 @@ eg.
   []
   (.addEventListener js/window "resize" resize-event-chan-handler))
 
+
+;;
+;; Frame Channel
+;; -------------
+;; frame channel receives a true every frame paint
+;;
+(def *frame-chans* (atom #{}))
+
+(defn new-frame-chan []
+  (let [c (chan)]
+    (swap! *frame-chans* conj c)
+    c))
+
+(defn del-frame-chan! [c]
+  (swap! *frame-chans* disj c))
+
+(defn clear-frame-chans! []
+  (swap! *frame-chans* #{}))
+
+(defn frame-event-chan-handler [ev]
+  (request-animation-frame frame-event-chan-handler)
+  (doseq [c @*frame-chans*] (put! c true)))
+
+(defn install-frame-handler
+  "install the frame callback to send frame chan messages"
+  []
+  (request-animation-frame frame-event-chan-handler))
