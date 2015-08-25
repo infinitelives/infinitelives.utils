7a5d6279f3368bf79dfd1dfbeca73bb71fa198e7
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-25 21:21:40 +0800

    add extra dom stuff so we could use it in fonts

--- src/cljs/infinitelives/utils/dom.cljs
+++ src/cljs/infinitelives/utils/dom.cljs
@@ -8,6 +8,12 @@
     (str (some-> (namespace s) (str "/")) (name s))
     s))
 
+(defn parent [elem]
+  (.-parentNode elem))
+
+(defn children [elem]
+  (.-children elem))
+
 (defn set-style!
   "Set the style of `elem` using key-value pairs:
       (set-style! elem :display \"block\" :color \"red\")"
@@ -23,3 +29,46 @@
   ([parent child]
      (doto parent
        (.appendChild child))))
+
+(defn create-element
+  ([tag]
+     (.createElement js/document (as-str tag)))
+  ([tag-ns tag]
+     (.createElementNS
+      js/document (as-str tag-ns) (as-str tag))))
+
+(defn insert-before!
+  "Insert `elem` before `other`, `other` must have a parent"
+  [elem other]
+  (let [p (parent other)]
+    (assert p "Target element must have a parent")
+    (.insertBefore p elem other)
+    elem))
+
+(defn set-attr!
+  "Sets dom attributes on and returns `elem`.
+   Attributes without values will be set to their name:
+       (set-attr! elem :disabled)
+   With values, the function takes variadic kv pairs:
+       (set-attr! elem :id \"some-id\"
+                       :name \"some-name\")"
+  ([elem k] (set-attr! elem k (as-str k)))
+  ([elem k v]
+     (let [k (as-str k)]
+       (when v
+         (if (fn? v)
+           (doto elem (aset k v))
+           (doto elem (.setAttribute k v))))))
+  ([elem k v & kvs]
+     (assert (even? (count kvs)))
+     (doseq [[k v] (->> kvs (partition 2) (cons [k v]))]
+       (set-attr! elem k v))
+     elem))
+
+(defn set-text!
+  "Set the textContent of `elem` to `text`, fall back to innerText"
+  [elem text]
+  (if-not (undefined? (.-textContent elem))
+    (set! (.-textContent elem) text)
+    (set! (.-innerText elem) text))
+  elem)
