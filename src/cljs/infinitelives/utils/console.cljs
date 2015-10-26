0ba1d80b426a77d4e933e5c6e8caacc4d5421574
Chris McCormick <chris@mccormick.cx>
2015-10-26 12:10:31 +0800

    Pass final argument to utils/log through to enable dropping the log statement temporarily into code easily to see what's going on during debugging.

--- src/cljs/infinitelives/utils/console.cljs
+++ src/cljs/infinitelives/utils/console.cljs
@@ -1,3 +1,7 @@
 (ns infinitelives.utils.console)
 
-(defn log [& args] (js/console.log.apply js/console (clj->js (into [] args))))
+(defn log [& args]
+  "Logs arguments to the js console and returns the last argument (passthrough)."
+  (js/console.log.apply js/console (clj->js (into [] args)))
+  (last args))
+
