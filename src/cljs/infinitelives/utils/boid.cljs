3e228c1f2b1ec842c4724cd6596e182687442f9f
Crispin Wellington <retrogradeorbit@gmail.com>
2016-02-25 22:26:17 +0800

    dry up code

--- src/cljs/infinitelives/utils/boid.cljs
+++ src/cljs/infinitelives/utils/boid.cljs
@@ -13,6 +13,16 @@
    :max-force 1.0
    :max-speed 4.0})
 
+(defn apply-steering [{:keys [mass vel max-speed] :as boid} steering]
+  (let [accel (vec2/scale steering (/ 1 mass))
+        new-vel (-> accel
+                    (vec2/add vel)
+                    (vec2/truncate max-speed))
+        new-pos (vec2/add pos new-vel)]
+    (assoc boid
+           :pos new-pos
+           :vel new-vel)))
+
 (defn seek [{:keys [mass pos vel max-force max-speed] :as boid} target]
   (let [desired-vel
         (-> pos
@@ -20,16 +30,8 @@
             (vec2/scale max-speed))
         steering (-> desired-vel
                      (vec2/sub vel)
-                     (vec2/truncate max-force))
-        accel (vec2/scale steering (/ 1 mass))
-        new-vel (-> accel
-                    (vec2/add vel)
-                    (vec2/truncate max-speed))
-
-        new-pos (vec2/add pos new-vel)]
-    (assoc boid
-           :pos new-pos
-           :vel new-vel)))
+                     (vec2/truncate max-force))]
+    (apply-steering boid steering)))
 
 (defn flee [{:keys [mass pos vel max-force max-speed] :as boid} target]
   (let [desired-vel
@@ -38,16 +40,8 @@
             (vec2/scale (- max-speed)))
         steering (-> desired-vel
                      (vec2/sub vel)
-                     (vec2/truncate max-force))
-        accel (vec2/scale steering (/ 1 mass))
-        new-vel (-> accel
-                    (vec2/add vel)
-                    (vec2/truncate max-speed))
-
-        new-pos (vec2/add pos new-vel)]
-    (assoc boid
-           :pos new-pos
-           :vel new-vel)))
+                     (vec2/truncate max-force))]
+    (apply-steering boid steering)))
 
 (defn pursue [{:keys [pos] :as boid} target target-vel]
   (seek boid (->> pos
