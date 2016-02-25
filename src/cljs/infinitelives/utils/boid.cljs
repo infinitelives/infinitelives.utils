c59a14ee39b1cf6a2426f6b01c35ec914b1ebda6
Crispin Wellington <retrogradeorbit@gmail.com>
2016-02-25 21:37:26 +0800

    bring in first general boid code. seek/flee

---
+++ src/cljs/infinitelives/utils/boid.cljs
@@ -0,0 +1,47 @@
+(ns infinitelives.utils.boid
+  (:require [infinitelives.utils.vec2 :as vec2]
+            [infinitelives.utils.math :as math]
+            [infinitelives.utils.console :refer [log]]))
+
+(def example
+  {:mass 10.0
+   :pos (vec2/vec2 0 0)
+   :vel (vec2/vec2 0 0)
+   :max-force 1.0
+   :max-speed 4.0})
+
+(defn seek [{:keys [mass pos vel max-force max-speed] :as boid} target]
+  (let [desired-vel
+        (-> pos
+            (vec2/direction target)
+            (vec2/scale max-speed))
+        steering (-> desired-vel
+                     (vec2/sub vel)
+                     (vec2/truncate max-force))
+        accel (vec2/scale steering (/ 1 mass))
+        new-vel (-> accel
+                    (vec2/add vel)
+                    (vec2/truncate max-speed))
+
+        new-pos (vec2/add pos new-vel)]
+    (assoc boid
+           :pos new-pos
+           :vel new-vel)))
+
+(defn flee [{:keys [mass pos vel max-force max-speed] :as boid} target]
+  (let [desired-vel
+        (-> pos
+            (vec2/direction target)
+            (vec2/scale (- max-speed)))
+        steering (-> desired-vel
+                     (vec2/sub vel)
+                     (vec2/truncate max-force))
+        accel (vec2/scale steering (/ 1 mass))
+        new-vel (-> accel
+                    (vec2/add vel)
+                    (vec2/truncate max-speed))
+
+        new-pos (vec2/add pos new-vel)]
+    (assoc boid
+           :pos new-pos
+           :vel new-vel)))
