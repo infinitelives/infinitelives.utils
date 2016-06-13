ddc8447a69113344ee3f912ab3d8a8731ccc3bbd
Crispin Wellington <retrogradeorbit@gmail.com>
2016-06-13 23:20:51 +0800

    groupid

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject infinitelives.utils "0.1.0-SNAPSHOT"
+(defproject infinitelives/infinitelives.utils "0.1.0-SNAPSHOT"
   :description "Support utilities for cljs games"
   :url "https://github.com/infinitelives"
   :license {:name "Eclipse Public License"
