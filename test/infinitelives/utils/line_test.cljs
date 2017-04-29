fd640b40c8d562e6937a70489ac50ccf5cfe2f3a
Crispin Wellington <retrogradeorbit@gmail.com>
2017-04-29 23:22:02 +0800

    line namespace test

---
+++ test/infinitelives/utils/line_test.cljs
@@ -0,0 +1,6 @@
+(ns infinitelives.utils.line-test
+  (:require [cljs.test :refer-macros [deftest is]]
+            [infinitelives.utils.line :as line]))
+
+(deftest bresenham
+  (is (= (line/bresenham 0 0 0 0) [[0 0]])))
