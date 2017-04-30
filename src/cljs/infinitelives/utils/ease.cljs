b9065b9b664c282cf19ef9486053a1124c3883af
Crispin Wellington <retrogradeorbit@gmail.com>
2017-04-30 14:16:45 +0800

    refactored ease functions

--- src/cljs/infinitelives/utils/ease.cljs
+++ src/cljs/infinitelives/utils/ease.cljs
@@ -9,31 +9,52 @@
 ;; b - initial value
 ;; c - change (final value - initial value)
 
+;; *-f functions take t in range 0 to 1
+;;     and return value v in range 0 to 1
+
+(def linear-f identity)
+
+(defn- quad-in-f [t]
+  (* t t))
+
+(defn- quad-out-f
+  "v = -(x-1)^2 + 1
+  =>   v = -x^2 + 2x
+  =>   v = x * (2-x)"
+
+  [t]
+  (* t (- 2 t)))
+
+(defn- quad-in-out-f [t]
+  (if (< t 0.5)
+    (* 2 t t)
+    (+ -1 (* t (- 4 (* 2 t))))))
+
+(defn- make-ease-fn [func]
+  (fn [t b c d]
+    (+ b (* c (func (/ t d))))))
+
 (defn linear [t b c d]
   (+ b (* c (/ t d))))
 
-(defn quad-in
-  "quadtratic curve, starting easy to start, then
-  speeding up to the destination"
-  [t b c d]
-  (let [rt (/ t d)]
-    (+ b (* c (* rt rt)))))
 
-(defn quad-out
-  "quadratic curve, starting fast, and then slowing
-  down to a stop at the destination."
-  [t b c d]
-  (let [rt (- (/ t d) 1)]
-    (+ b (* c (- 1 (* rt rt))))))
+(def
+  ^{:arglist '([t b c d])
+    :doc "quadtratic curve, starting easy to start, then
+  speeding up to the destination"}
 
-(defn quad-in-out
-  "two quadratic curves: starting easy to start, then
+  quad-in (make-ease-fn quad-in-f))
+
+(def
+  ^{:arglist '([t b c d])
+    :doc "quadratic curve, starting fast, and then slowing
+  down to a stop at the destination."}
+  quad-out (make-ease-fn quad-out-f))
+
+(def
+  ^{:arglist '([t b c d])
+    :doc "two quadratic curves: starting easy to start, then
   speeding up to the halfway point, then continuing
   fast through the halfway point and then slowing down
-  to stop at the destination"
-  [t b c d]
-  (let [hd (/ d 2)
-        hc (/ c 2)]
-    (if (< t hd)
-      (quad-in t b hc hd)
-      (quad-out (- t hd) (+ b hc) hc hd))))
+  to stop at the destination"}
+  quad-in-out (make-ease-fn quad-in-out-f))
